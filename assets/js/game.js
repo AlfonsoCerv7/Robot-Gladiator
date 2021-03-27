d7c01825be1fdc5bdec10f0c5af3da0ee78aa742
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-03-27 13:57:23 -0500

    started the js for robot gladiator

---
+++ assets/js/game.js
@@ -0,0 +1,30 @@
+function fight() {
+    window.alert("The fight has begun!");
+    }
+    var playerName = window.prompt("What is your robot's name?");
+    // Note the lack of quotation marks around playerName
+    window.alert(playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fight();
